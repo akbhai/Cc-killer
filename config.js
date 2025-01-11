42e3a3b79a73a0e1f3c38448624aeaebadce735f
akbhai <34899561+akbhai@users.noreply.github.com>
2025-01-12 00:45:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     domains: ["lamoshitt.com", "crazzybabe.com"], // names of domain for the emails. 
     oneTimeMailGen: 10 * 100000,
     telegramBotToken: "8027446535:AAFFznr39l-PkbQ3M4vCX7JBnjkB3nmzJO8", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
-    adminNamesForTelegramBot: ["akbhai007","alen_heree"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
-    authorisedUserForTelegramBot: ["akbhai007","alen_heree"] // username or id of the user who can use the bot.
+    adminNamesForTelegramBot: ["akbhai007"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+    authorisedUserForTelegramBot: ["akbhai007"] // username or id of the user who can use the bot.
 };
